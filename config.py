e6ca8692b67622dd01877d2546e799e978509d07
ashkan <bagheriashkan77@gmail.com>
2022-10-01 17:46:25 +0330

    Add files via upload

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD = [0.229, 0.224, 0.225]
 DATASET_DIR = 'dataset'
 CHECK_DIR = 'checkpoints'
-MODEL_NAME = 'UNet'
+MODEL_NAME = 'DeepLabV3Plus'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
